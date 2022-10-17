1d0718847705c62e14dd11e8876e4ffd6d92fc67
Ezequiel Mestelan <elmeste.88@gmail.com>
2022-10-17 15:12:49 -0300

    Modificacion en funcion login

--- app/controllers/authController.php
+++ app/controllers/authController.php
@@ -3,23 +3,30 @@
     class AuthController extends MainController {
 
         function login(){
-
-            //if email exists, get values.
-            if (isset($_POST['email'])) {
-                $email = $_POST['email'];
-                $password = $_POST['password'];
-            }
-
-            //get user by email.
-            $user = $this->authModel->getUser($email);
-
-            //check if user and password are correct.
-            if ($user && password_verify($password, $user->password)) {
-                $_SESSION['name'] = $user->userName;
-                $_SESSION['email'] = $user->email;
-                header('Location: ' . BASE_URL);
+            if(!isset($this->name)){
+                //if email exists, get values.
+                if (isset($_POST['email'])) {
+                    $email = $_POST['email'];
+                    $password = $_POST['password'];
+                }
+    
+                //get user by email.
+                if(isset($email)){
+                    $user = $this->authModel->getUser($email);
+        
+                    //check if user and password are correct.
+                    if ($user && password_verify($password, $user->password)) {
+                        $_SESSION['name'] = $user->userName;
+                        $_SESSION['email'] = $user->email;
+                        header('Location: ' . BASE_URL);
+                    } else {
+                        $this->userView->showError("Usuario o contraseña incorrectos", $this->name);
+                    }
+                } else {
+                    $this->userView->showError("Usuario o contraseña incorrectos", $this->name);
+                }
             } else {
-                $this->userView->showError("Usuario o contraseña incorrectos", $this->name);
+                $this->userView->showError("Admin ya se encuentra logueado");
             }
         }
 
